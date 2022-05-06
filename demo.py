3501468770cb27e4518a2f7aa50bfdbec250204a
haohao <haohaolalahao@iCloud.com>
2022-05-06 21:52:30 +0800

    🚧

--- demo.py
+++ demo.py
@@ -45,7 +45,7 @@ payload_1 = {
 
 
 
-async def main():
+async def main_1():
     async with aiohttp.ClientSession(headers=headers, cookies=cookie) as session:
         async with session.post(submit_url, data=payload) as response:
             print('1-1', pendulum.now())
@@ -65,5 +65,43 @@ async def main():
             print(data)
             print('2-2', pendulum.now())
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+
+async def fetch(session, url, payload):
+    async with session.post(url, data=payload) as resp:
+        if resp.status != 200:
+            resp.raise_for_status()
+        data = await resp.json()
+        print(data)
+        print(pendulum.now())
+        return data
+
+
+async def fetch_multi(session, url, payloads):
+    tasks = []
+    for payload in payloads:
+        task = asyncio.create_task(fetch(session, url, payload))
+        tasks.append(task)
+    # gather: 搜集所有future对象，并等待返回
+    results = await asyncio.gather(*tasks)
+    return results
+
+
+async def main():
+    url = "http://gym.dazuiwl.cn/api/order/submit"
+    payloads = [payload, payload_1]
+    # tcp 连接池
+    conn = aiohttp.TCPConnector(limit=3)
+    # async with aiohttp.ClientSession(connector=conn) as session:
+    async with aiohttp.ClientSession(headers=headers, cookies=cookie, connector=conn) as session:
+        datas = await fetch_multi(session, submit_url, payloads)
+        print(datas)
+
+
+if __name__ == '__main__':
+    # asyncio.run(main())
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
+    loop_1 = asyncio.get_event_loop()
+    loop_1.run_until_complete(main_1())
--- submit.py
+++ submit.py
@@ -8,17 +8,18 @@ from rich import print
 from get_status import check_bm_status
 from get_info import check_court_status
 
+
 def submit(
     token: str = "",
     cookie="",
     sports: str = "bm",
     day: str = "tomorrow",
-    court_1: str = '1',
-    book_time_1_1: str = '8',
-    book_time_1_2: str = '9',
-    court_2: str = '2',
-    book_time_2_1: str = '8',
-    book_time_2_2: str = '9',
+    court_1: str = "1",
+    book_time_1_1: str = "8",
+    book_time_1_2: str = "9",
+    court_2: str = "2",
+    book_time_2_1: str = "8",
+    book_time_2_2: str = "9",
 ):
     """
     pingpang 33 badminton 34
@@ -41,14 +42,23 @@ def submit(
     # pay
     submit_url = "http://gym.dazuiwl.cn/api/order/submit"
 
-    courts = [
-        '155', '156', '157', '158', '159', '160', '161', '162', '163', '164',
-        '165', '166'
-    ]
+    courts = ["155", "156", "157", "158", "159", "160", "161", "162", "163", "164", "165", "166"]
     court_times = [
-        '328101', '328102', '328103', '328104', '328105', '328106', '328107',
-        '328125', '328126', '328127', '328128', '328129', '328130', '328131',
-        '328132'
+        "328101",
+        "328102",
+        "328103",
+        "328104",
+        "328105",
+        "328106",
+        "328107",
+        "328125",
+        "328126",
+        "328127",
+        "328128",
+        "328129",
+        "328130",
+        "328131",
+        "328132",
     ]
 
     # first group
@@ -71,24 +81,22 @@ def submit(
 
     # first group
     court_index_1 = int(eval(court_1))
-    assert 1 <= court_index_1 <= 12, 'bad court number'
+    assert 1 <= court_index_1 <= 12, "bad court number"
     book_time_index_1_1 = int(eval(book_time_1_1))
-    assert 7 <= book_time_index_1_1 <= 21, 'bad book time first'
+    assert 7 <= book_time_index_1_1 <= 21, "bad book time first"
 
-    if book_time_1_2 == '':
+    if book_time_1_2 == "":
         book_time_index_1_2 = False
     else:
         book_time_index_1_2 = int(eval(book_time_1_2))
-        assert 7 <= book_time_index_1_2 <= 21, 'bad book time second'
+        assert 7 <= book_time_index_1_2 <= 21, "bad book time second"
 
     court_real_1 = courts[court_index_1 - 1]
     if book_time_index_1_2:
-        book_time_1 = court_times[book_time_index_1_1 -
-                                  7] + ', ' + court_times[book_time_index_1_2 -
-                                                          7]
+        book_time_1 = court_times[book_time_index_1_1 - 7] + ", " + court_times[book_time_index_1_2 - 7]
     else:
         book_time_1 = court_times[book_time_index_1_1 - 7]
-    scene_1 = '[{"day":"' + f'{day}' + '","fields":{"' + f'{court_real_1}' + '":[' + f'{book_time_1}' + ']}}]'
+    scene_1 = '[{"day":"' + f"{day}" + '","fields":{"' + f"{court_real_1}" + '":[' + f"{book_time_1}" + "]}}]"
     payload_1 = {
         "orderid": "",
         "card_id": "",
@@ -102,24 +110,22 @@ def submit(
 
     # second group
     court_index_2 = int(eval(court_2))
-    assert 1 <= court_index_2 <= 12, 'bad court number'
+    assert 1 <= court_index_2 <= 12, "bad court number"
     book_time_index_2_1 = int(eval(book_time_2_1))
-    assert 7 <= book_time_index_2_1 <= 21, 'bad book time first'
+    assert 7 <= book_time_index_2_1 <= 21, "bad book time first"
 
-    if book_time_2_2 == '':
+    if book_time_2_2 == "":
         book_time_index_2_2 = False
     else:
         book_time_index_2_2 = int(eval(book_time_2_2))
-        assert 7 <= book_time_index_2_2 <= 21, 'bad book time second'
+        assert 7 <= book_time_index_2_2 <= 21, "bad book time second"
 
     court_real_2 = courts[court_index_2 - 1]
     if book_time_index_2_2:
-        book_time_2 = court_times[book_time_index_2_1 -
-                                  7] + ', ' + court_times[book_time_index_2_2 -
-                                                          7]
+        book_time_2 = court_times[book_time_index_2_1 - 7] + ", " + court_times[book_time_index_2_2 - 7]
     else:
         book_time_2 = court_times[book_time_index_2_1 - 7]
-    scene_2 = '[{"day":"' + f'{day}' + '","fields":{"' + f'{court_real_2}' + '":[' + f'{book_time_2}' + ']}}]'
+    scene_2 = '[{"day":"' + f"{day}" + '","fields":{"' + f"{court_real_2}" + '":[' + f"{book_time_2}" + "]}}]"
     payload_2 = {
         "orderid": "",
         "card_id": "",
@@ -137,8 +143,7 @@ def submit(
         token = "ae1660ad-042d-4293-b4e8-6706a314b5ee"
 
     headers = {
-        "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.50",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.50",
         "token": token,
         "Host": "gym.dazuiwl.cn",
         "Origin": "http://gym.dazuiwl.cn",
@@ -151,86 +156,80 @@ def submit(
 
     if cookie == "":
         cookie = {"PHPSESSID": "392enpsjdnre0svp25m8e31cmm"}
-    
+
     # NOTE check cookies & token
     try:
-        res = requests.post(submit_url,
-                            headers=headers,
-                            data=payload_1,
-                            cookies=cookie,
-                            timeout=5).json()
+        res = requests.post(submit_url, headers=headers, data=payload_1, cookies=cookie, timeout=5).json()
     except:
-        print('please change cookie and token')
+        print("please change cookie and token")
         return 0
 
     # NOTE check courts
     try:
-        status_1 = check_court_status(day=day, court=court_1, book_time_1=book_time_1_1, book_time_2=book_time_1_2, print_info=True)
+        status_1 = check_court_status(
+            day=day, court=court_1, book_time_1=book_time_1_1, book_time_2=book_time_1_2, print_info=True
+        )
         if status_1 != 1:
-           return 0
-        status_2 = check_court_status(day=day, court=court_2, book_time_1=book_time_2_1, book_time_2=book_time_2_2, print_info=True)
+            return 0
+        status_2 = check_court_status(
+            day=day, court=court_2, book_time_1=book_time_2_1, book_time_2=book_time_2_2, print_info=True
+        )
         if status_2 != 1:
-           return 0
+            return 0
     except:
-        print('check field')
+        print("check field")
         return 0
 
     today = pendulum.today().to_date_string()
-    over_time = pendulum.parser.parse(today + 'T07:20:00+08:00')
-    begin_time = pendulum.parser.parse(today + 'T06:59:55+08:00')
+    over_time = pendulum.parser.parse(today + "T07:20:00+08:00")
+    begin_time = pendulum.parser.parse(today + "T06:59:55+08:00")
     flag = 0
 
     while True:
         # Time judge
         now = pendulum.now()
-        print(':timer_clock: ', now)
+        print(":timer_clock: ", now)
 
         if now >= begin_time:
 
             # TODO 异步
-            """ 
-                从6:59:55开始到7:20开始, async 异步实现同时发出200个左右的请求, 每个场地请求100次
-                需要记录一下单词api时间，timeout很重要，要保持和服务器的连接
             """
-            
+            从6:59:55开始到7:20开始, async 异步实现同时发出200个左右的请求, 每个场地请求100次
+            需要记录一下单词api时间, timeout很重要, 要保持和服务器的连接
+            """
+
             try:
-                res_1 = requests.post(submit_url,
-                                      headers=headers,
-                                      data=payload_1,
-                                      cookies=cookie,
-                                      timeout=10).json()
-                res_2 = requests.post(submit_url,
-                                      headers=headers,
-                                      data=payload_2,
-                                      cookies=cookie,
-                                      timeout=10).json()
-                if res_1['code'] == 1 or res_2['code'] == 1:
+                res_1 = requests.post(submit_url, headers=headers, data=payload_1, cookies=cookie, timeout=10).json()
+                res_2 = requests.post(submit_url, headers=headers, data=payload_2, cookies=cookie, timeout=10).json()
+                if res_1["code"] == 1 or res_2["code"] == 1:
                     flag = 1
                     break
                 pass
-            
+
             except:
-                print('try again')
+                print("try again")
         else:
             time.sleep(0.5)
-
 
         if now >= over_time:
             break
 
     if flag == 1:
-        print(':rocket: success')
+        print(":rocket: success")
         return 1
     else:
-        print(':loudly_crying_face: field')
+        print(":loudly_crying_face: field")
         return 0
 
+
 if __name__ == "__main__":
-    submit(sports="bm",
-           day="tomorrow",
-           court_1="1",
-           book_time_1_1="10",
-           book_time_1_2="11",
-           court_2="2",
-           book_time_2_1="10",
-           book_time_2_2="11")
+    submit(
+        sports="bm",
+        day="tomorrow",
+        court_1="1",
+        book_time_1_1="10",
+        book_time_1_2="11",
+        court_2="2",
+        book_time_2_1="10",
+        book_time_2_2="11",
+    )
